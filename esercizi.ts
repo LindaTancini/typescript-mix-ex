194044ad4fd2970e455652b0ea96d46c24a8da43
LindaTancini <lindatancini95@gmail.com>
2025-07-08 11:42:25 +0200

    feat: ex typescript config

---
+++ esercizi.ts
@@ -0,0 +1,29 @@
+// Tipi di base
+
+// Esercizio 1
+console.log("Ciao");
+// Esercizio 2
+
+// Esercizio 3
+
+// Interfacce e tipi personalizzati
+
+// Esercizio 4
+
+// Esercizio 5
+
+// Esercizio 6
+
+// Programmazione a Oggetti (OOP)
+
+// Esercizio 7
+
+// Esercizio 8
+
+// Esercizio 9
+
+// Type Inference e controllo dei tipi
+
+// Esercizio 10
+
+// Esercizio 11

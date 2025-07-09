24e52322ec3b8491bf8421a97bb56ae8b9607141
LindaTancini <lindatancini95@gmail.com>
2025-07-09 10:23:46 +0200

    feat: tipi base

--- esercizi.ts
+++ esercizi.ts
@@ -1,10 +1,31 @@
 // Tipi di base
 
-// Esercizio 1
-console.log("Ciao");
-// Esercizio 2
+// Esercizio 1 (Crea variabili di tipo string, number, boolean, array, e tuple. Prova ad assegnare valori errati per osservare gli errori.)
+{
+  let nome: string;
+  nome = "Linda";
+  let eta: number;
+  eta = 29;
+  let isFemale: boolean;
+  isFemale = true;
+  const numbers: number[] = [1, 2, 3, 4]; // array
+  let persona: [string, number] = ["Linda", 29]; // tuple
+}
+// Esercizio 2 (Scrivi una funzione saluta che accetta un parametro nome: string e ritorna "Ciao, nome".)
+{
+  function saluta(nome: string): string {
+    return `Ciao, ${nome}`;
+  }
+  console.log(saluta("Linda"));
+}
 
-// Esercizio 3
+// Esercizio 3 (Crea una funzione calcolaSomma che accetta due numeri e restituisce la somma.)
+{
+  function somma(n1: number, n2: number): void {
+    console.log(n1 + n2);
+  }
+  somma(2, 2);
+}
 
 // Interfacce e tipi personalizzati
 

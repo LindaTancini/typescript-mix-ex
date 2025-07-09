8ec3c594bbf583f0d7e6196bfa3e0d4a09f61c4c
LindaTancini <lindatancini95@gmail.com>
2025-07-09 12:07:44 +0200

    feat: ex ts config 02

---
+++ 02/esercizi.ts
@@ -0,0 +1,65 @@
+// Tipi di base e Funzioni
+
+// Esercizio 1
+
+// Esercizio 2
+
+// Esercizio 3
+
+// Array, Tuple e Union Types
+
+// Esercizio 4
+
+// Esercizio 5
+
+// Esercizio 6
+
+// Interfacce e Oggetti
+
+// Esercizio 7
+
+// Esercizio 8
+
+// Esercizio 9
+
+//  Tipi Avanzati: Type Alias, Union, Literal Types
+
+// Esercizio 10
+
+// Esercizio 11
+
+// Esercizio 12
+
+// Classi e OOP
+
+// Esercizio 13
+
+// Esercizio 14
+
+// Esercizio 15
+
+// Modificatori di accesso
+
+// Esercizio 16
+
+// Esercizio 17
+
+// Esercizio 18
+
+// Type Inference e controllo dei tipi
+
+// Esercizio 19
+
+// Esercizio 20
+
+// Funzioni con parametri opzionali e default
+
+// Esercizio 21
+
+// Esercizio 22
+
+// Oggetti come parametri
+
+// Esercizio 23
+
+// Esercizio 24

cd1d40b9f6e608b10c1f56de8e10deecefdb862b
LindaTancini <lindatancini95@gmail.com>
2025-07-10 15:50:58 +0200

    feat: ex ts config 03

---
+++ 03/esercizi.ts
@@ -0,0 +1,21 @@
+// Tipi Avanzati
+
+// Esercizio 1.1 – Stato ordine
+
+// Esercizio 1.2 – Risultato
+
+// Esercizio 1.3 – Contatto
+
+// Classi e OOP
+
+// Esercizio 2.1 – Classe Veicolo
+
+// Esercizio 2.2 – Classe Auto (estende Veicolo)
+
+// Esercizio 2.3 – Animale, Cane, Gatto
+
+// Modificatori di accesso
+
+// Esercizio 3.1 – Conto Corrente
+
+// Esercizio 3.2 – Utente con protected

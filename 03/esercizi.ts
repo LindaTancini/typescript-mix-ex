1f3c0a76a70709c68cf0e69807a849bf5a1d3d85
LindaTancini <lindatancini95@gmail.com>
2025-07-11 11:04:43 +0200

    feat: tipi avanzati

--- 03/esercizi.ts
+++ 03/esercizi.ts
@@ -1,10 +1,52 @@
 // Tipi Avanzati
 
 // Esercizio 1.1 – Stato ordine
+{
+  type StatoOrdine = "in attesa" | "spedito" | "consegnato" | "annullato";
+  function aggiornaStato(id: number, stato: StatoOrdine): void {
+    if (stato === "in attesa") {
+      console.log("L'ordine è in fase di attesa");
+    } else if (stato === "spedito") {
+      console.log("L'ordine è stato spedito");
+    } else if (stato === "consegnato") {
+      console.log("L'ordine è stato consegnato");
+    } else if (stato === "annullato") {
+      console.log("L'ordine è stato annullato");
+    }
+  }
+  const id = 101;
+  aggiornaStato(id, "spedito");
+}
 
 // Esercizio 1.2 – Risultato
+{
+  type Risultato = string | number;
+  function stampaRisultato(r: Risultato): void {
+    if (typeof r === "string") {
+      console.log("Sono una stringa");
+    } else if (typeof r === "number") {
+      console.log("Sono un numero");
+    }
+  }
+  stampaRisultato("ciao");
+  stampaRisultato(8);
+}
 
 // Esercizio 1.3 – Contatto
+{
+  type Contatto =
+    | { tipo: "email"; valore: string }
+    | { tipo: "telefono"; valore: number };
+  function mostraContatto(contatto: Contatto): void {
+    if (contatto.tipo === "email") {
+      console.log("Email:", contatto.valore);
+    } else if (contatto.tipo === "telefono") {
+      console.log("Telefono:", contatto.valore);
+    }
+  }
+  mostraContatto({ tipo: "email", valore: "utente@example.com" });
+  mostraContatto({ tipo: "telefono", valore: 1234567890 });
+}
 
 // Classi e OOP
 

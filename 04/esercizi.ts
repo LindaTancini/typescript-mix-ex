7df86487f8f1774aa4715b98a3310d15b7c580bc
LindaTancini <lindatancini95@gmail.com>
2025-07-12 10:48:27 +0200

    feat: ex ts config 4

---
+++ 04/esercizi.ts
@@ -0,0 +1,15 @@
+// OOP
+
+// Esercizio 1 – Gestione animali
+
+// Esercizio 2 – Sistema scolastico
+
+// Esercizio 3 – Biblioteca
+
+// Modificatori di accesso
+
+// Esercizio 4 – Conto bancario avanzato
+
+// Esercizio 5 – Gerarchia utenti
+
+// Esercizio 6 – Registro accessi (Porta)
